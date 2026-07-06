fe440b72e9c6180a2ffc66b287333ac953b54492
istiakSAAD007 <s12a12saadabir@gmail.com>
2026-07-06 12:10:18 +0600

    discussion controller...

--- src/controllers/discussionController.js
+++ src/controllers/discussionController.js
@@ -1,5 +1,30 @@
 import prisma from "../configs/prisma.js";
 
+export const createPost = async (req, res) => {
+  const { positionId } = req.params;
+  const { content } = req.body;
+
+  try {
+    if (!content)
+      return res.status(400).json({ error: "Post content is required." });
+
+    const newPost = await prisma.discussion.create({
+      data: {
+        positionId,
+        authorId: req.user.userId,
+        content,
+      },
+      include: {
+        author: { select: { firstName: true, lastName: true, role: true } },
+      },
+    });
+
+    return res.status(201).json({ success: true, post: newPost });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to post message." });
+  }
+};
+
 export const getDiscussions = async (req, res) => {
   const { positionId } = req.params;
 
